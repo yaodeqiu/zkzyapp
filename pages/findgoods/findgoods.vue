1e3714ebf9ffa96f9bcab05d57396f04855ddbef
YA <1285164319@qq.com>
2019-10-21 15:42:33 +0800

    提交项目架构

---
+++ pages/findgoods/findgoods.vue
@@ -0,0 +1,25 @@
+<template>
+	<view>
+		<uni-drawer :visible="false">
+		    <view style="padding:30upx;">
+		        <view class="uni-title">抽屉式导航</view>
+		    </view>
+		</uni-drawer>
+	</view>
+</template>
+
+<script>
+    import {uniDrawer} from '@dcloudio/uni-ui'
+ 	export default {
+		components: {uniDrawer},
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style>
+	
+</style>
---
+++ pages/index/index.vue
@@ -0,0 +1,37 @@
+<template>
+	<view class="content">
+		<view>
+		     <a @tap="goFindgoods">找货</a>
+			 <a @tap="goSendgoods">发货</a>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: '这是首页'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+            goFindgoods(){
+				uni.navigateTo({
+				    url:'../findgoods/findgoods',
+				});
+			},
+			goSendgoods(){
+				uni.navigateTo({
+				    url:'../sendgoods/sendgoods',
+				});
+			},
+		}
+	}
+</script>
+
+<style>
+
+</style>
